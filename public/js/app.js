9ae8e0657ddee31a4ded9a9a7182980fe478eddc
Zoe <zoereagan@gmail.com>
2018-04-20 10:51:52 -0500

    created login in view page login/registration forms

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+console.log("connected");
+
+$('a').on('click', (e) => {
+    console.log(e.currentTarget.innerText);
+
+    if(e.currentTarget.innerText === 'Login'){
+
+        if($('#login').hasClass('invisible')){
+           $('#login').toggleClass('invisible');
+           $('#registration').toggleClass('invisible')
+
+        }
+
+    } else if (e.currentTarget.innerText === 'Registration'){
+
+      if($('#registration').hasClass('invisible')){
+        $('#registration').toggleClass('invisible')
+        $('#login').toggleClass('invisible')
+      }
+    } else {
+      console.log("do nothing");
+    }
+});
