8e74c5494b33d599297f90c2503d82514e547ced
Zoe <zoereagan@gmail.com>
2018-04-20 14:23:57 -0500

    worked on artist controller

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -18,18 +18,7 @@ router.get('/', async (req, res, next) => {
 		}
 })
 
-// get edit route
-
-router.get('/:id/edit', async (req, res, next) => {
-	try {
-		res.render('artist/edit.ejs');
-	} catch (err) {
-		next(err);
-	}
-})
-
 // get show route
-
 router.get('/:id', async (req, res, next) => {
 	try {
 		res.render('artist/show.ejs');
@@ -38,7 +27,53 @@ router.get('/:id', async (req, res, next) => {
 	}
 })
 
-// get new route
+
+//new artist post route
+router.post('/new', async (req, res, next) => {
+//need to use body-parser
+//also, properties in schema and input form MUST MATCH
+
+  try {
+    const createdArtist = await Artist.create(req.body);
+    res.redirect('artist/new.ejs');
+  } catch(err) {
+    next(err)
+  }
+});
+
+
+// // get edit route
+//
+// router.get('/:id/edit', async (req, res, next) => {
+// 	try {
+// 		res.render('artist/edit.ejs');
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// })
+//
+//
+// // put edit route
+// router.put("/:id", async (req, res, next) => {
+// 	try {
+// 		const artistEdit = {};
+//     artistEdit.name= req.body.name;
+//     artistEdit.genre = req.body.genre;
+// 		artistEdit.location = req.body.location;
+// 		artistEdit.website = req.body.website;
+// 		artistEdit.imglink = req.body.imglink;
+//
+// 		const updatedArtist = await Artist.findByIdAndUpdate(req.params.id, req.body);
+//
+// 		res.redirect("/artist");
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// })
+
+
+
+
 
 
 
