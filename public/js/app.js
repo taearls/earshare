2e9ca11f2d757554de91a20fb34fee2c582bf0bf
Zoe <zoereagan@gmail.com>
2018-04-23 10:26:14 -0500

    changed auth page login/register modals

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,26 @@
 // console.log("connected");
 
-$('a').on('click', (e) => {
-    // console.log(e.currentTarget.innerText);
-
-    if(e.currentTarget.innerText === 'Login'){
-
-        if($('#login').hasClass('invisible')){
-           $('#login').toggleClass('invisible');
-           $('#registration').toggleClass('invisible')
-
-        }
-
-    } else if (e.currentTarget.innerText === 'Register'){
-
-      if($('#registration').hasClass('invisible')){
-        $('#registration').toggleClass('invisible')
-        $('#login').toggleClass('invisible')
-      }
-    } else {
-      // console.log("do nothing");
-    }
-});
+// $('a').on('click', (e) => {
+//     // console.log(e.currentTarget.innerText);
+//
+//     if(e.currentTarget.innerText === 'Login'){
+//
+//         if($('#login').hasClass('invisible')){
+//            $('#login').toggleClass('invisible');
+//            $('#registration').toggleClass('invisible')
+//
+//         }
+//
+//     } else if (e.currentTarget.innerText === 'Register'){
+//
+//       if($('#registration').hasClass('invisible')){
+//         $('#registration').toggleClass('invisible')
+//         $('#login').toggleClass('invisible')
+//       }
+//     } else {
+//       // console.log("do nothing");
+//     }
+// });
 
 
 // make any image invisible if there's no src attribute added
@@ -33,5 +33,10 @@ $('img[src=""]').hide();
 //modals
 
 $('#loginModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+
+$('#registerModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
