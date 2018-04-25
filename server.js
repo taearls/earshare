7963befe2f70747867b6e80978e8f6e8cde38ac5
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-25 14:20:48 -0500

    updated carousel; only index views are accessible without being logged in

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
 	// check where user is going using req.path
 	// if we do artist/ and event/, someone could view artist and event index page without being logged in
-	if (req.path.includes('artist/') || req.path.includes('event/')){
+	if (req.path.includes('artist/') || req.path.includes('event/') || req.path.includes('user/')){
 		if (req.session.loggedIn) {
 			next();
 		} else {
