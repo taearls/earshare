51569927367d1163a02210bf014dfcd40943fb00
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-19 14:22:02 -0500

    now user images show up on landing page carousel

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,6 @@ $('#createNewEvent').on('click', e => {
 
 
 // make any image invisible if there's no src attribute added
-// $('img[src=""]').hide();
 $('img[src=""]').attr('src', "../images/missing-image.png")
 
 
