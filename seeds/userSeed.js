46db66751d55de595ec25f71f390a5fe21eef93b
Tyler Earls <tyler.a.earls@gmail.com>
2018-08-26 16:16:05 -0500

    fixed avatar links

--- seeds/userSeed.js
+++ seeds/userSeed.js
@@ -14,7 +14,7 @@ const seededUsers = [
     username: 'tboneearls',
     email: 'tboneearls@gmail.com',
     password: '$2b$10$jthGk9yLigxOa/Ic55k8zev7mMN8GQTopOkFIblZKFD3ygp/o0N9S', // tboneearls
-    avatar: 'https://amp.businessinsider.com/images/59e5d4a1b0c292755836e078-750-562.jpg',
+    avatar: 'https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/17499154_10206541228449135_5610285631864985330_n.jpg?_nc_cat=0&oh=9dcaeba2a4afb8ac8a353d9c5d026a34&oe=5C36048E',
     userConfirmed: true,
     artists: [],
     artistsLiked: [],
@@ -24,7 +24,7 @@ const seededUsers = [
     username: 'tyler',
     email: 'tyler.a.earls@gmail.com',
     password: '$2b$10$Wp3SKJTi6WVFg2dKjbsPkuCBJLpwr9Ip4hYrfxtebuLia3c0gh5Hi', // tyler
-    avatar: 'https://data.whicdn.com/images/14922648/large.jpg',
+    avatar: 'https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/36733990_10209366988931381_4424548404940505088_o.jpg?_nc_cat=0&oh=69fb9f49fc28c1dfca7ab088c4832412&oe=5C328A65',
     userConfirmed: false,
     artists: [],
     artistsLiked: [],
